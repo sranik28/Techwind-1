e146a821b61a1275303af5b0a9ba1c3143000338
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-09-12 04:51:45 +0600

    development UI update

--- src/components/home/Development.jsx
+++ src/components/home/Development.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import icon from '../../assets/change/correct.png'
 import { HiChevronRight } from 'react-icons/hi';
 import development from '../../assets/development/Mobile_notification_SVG.png'
 import DevFrom from '../DevFrom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 
 const Development = () => {
     return (
@@ -10,12 +11,12 @@ const Development = () => {
             <div className='grid items-center gap-12 md:grid-cols-2'>
                 <div>
                     <h2 className='mb-6 text-2xl font-semibold leading-normal md:text-3xl md:leading-normal'>Speed up your development
-                        with Techwind.</h2>
+                        with <span className='text-primary'>Techwind</span> .</h2>
                     <p className='max-w-xl text-slate-400'>You can combine all the Techwind templates into a single one, you can take a component from the Application theme and use it in the Website.</p>
                     <ul className='mt-6'>
-                        <li className='flex items-center gap-2 my-3 text-slate-400'><span><img className='w-4 ' src={icon} alt="" /></span> <p>Digital Marketing Solutions for Tomorrow</p></li>
-                        <li className='flex items-center gap-2 my-3 text-slate-400'><span><img className='w-4' src={icon} alt="" /></span><p>Our Talented & Experienced Marketing Agency</p></li>
-                        <li className='flex items-center gap-2 my-3 text-slate-400'><span><img className='w-4' src={icon} alt="" /></span><p>Create your own skin to match your brand</p></li>
+                        <li className='flex items-center gap-2 my-3 text-slate-400'><span><FontAwesomeIcon icon={faCircleCheck} className='text-primary' /></span> <p>Digital Marketing Solutions for Tomorrow</p></li>
+                        <li className='flex items-center gap-2 my-3 text-slate-400'><span><FontAwesomeIcon icon={faCircleCheck} className='text-primary' /></span><p>Our Talented & Experienced Marketing Agency</p></li>
+                        <li className='flex items-center gap-2 my-3 text-slate-400'><span><FontAwesomeIcon icon={faCircleCheck} className='text-primary' /></span><p>Create your own skin to match your brand</p></li>
                     </ul>
                     <button className='flex items-center gap-1 mt-6 text-lg text-primary'>Find Out More <span> <HiChevronRight className='text-xl' /> </span></button>
                 </div>
