e1893441b36b3d267e0a8ddab03902a7061f5269
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-09-12 00:23:48 +0600

    How work added

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Success from './Success';
 import Brand from './Brand';
+import HowWork from './HowWork';
 
 const HomePage = () => {
     return (
         <div>
             <Success />
             <Brand />
+            <HowWork />
         </div>
     );
 };
--- src/components/home/HowWork.jsx
+++ src/components/home/HowWork.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const HowWork = () => {
     return (
-        <div className='container'>
-            <div>
-                <h2>How It Work ?</h2>
-                <p>Start working with Tailwind CSS that can provide everything you need to generate awareness, drive traffic, connect.</p>
+        <div className='container mb-16 mt-36'>
+            <div className='text-center'>
+                <h2 className='mb-6 text-2xl font-semibold leading-normal md:text-3xl md:leading-normal'>How It Work ?</h2>
+                <p className='max-w-xl mx-auto text-slate-400'>Start working with Tailwind CSS that can provide everything you need to generate awareness, drive traffic, connect.</p>
             </div>
         </div>
     );
