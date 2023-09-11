ccc7a9c546e42d5ce63f74919881d5cbbb4c381c
MD Anik <122194751+sranik28@users.noreply.github.com>
2023-09-12 05:24:20 +0600

    Success icon added

--- src/components/home/Success.jsx
+++ src/components/home/Success.jsx
@@ -14,7 +14,10 @@ const Success = () => {
                     <button className='inline-flex items-center px-5 py-2 mt-2 text-base font-semibold tracking-wide text-center text-white align-middle duration-500 border border-indigo-600 rounded-md hover:bg-indigo-700 hover:border-indigo-700 me-2 rb-zeplin-selected bg-primary '><span className='mr-2'>
                         <AiOutlineMail />
                     </span> Get Started</button>
-                    <button className='inline-flex items-center px-5 py-2 mt-2 text-base font-semibold tracking-wide text-center text-indigo-600 align-middle duration-500 bg-transparent border border-indigo-600 rounded-md hover:bg-primary hover:text-white'>Documentation</button>
+                    <button className='inline-flex items-center px-5 py-2 mt-2 text-base font-semibold tracking-wide text-center text-indigo-600 align-middle duration-500 bg-transparent border border-indigo-600 rounded-md hover:bg-primary hover:text-white'>
+                        <span>
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="me-2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M19 2H6c-1.206 0-3 .799-3 3v14c0 2.201 1.794 3 3 3h15v-2H6.012C5.55 19.988 5 19.806 5 19s.55-.988 1.012-1H21V4c0-1.103-.897-2-2-2zm0 14H5V5c0-.806.55-.988 1-1h13v12z"></path></svg>
+                        </span> Documentation</button>
                 </div>
                 <div>
                     <img src={bannerImg} className='w-[450px] object-cover h-[100%]' alt="" />
